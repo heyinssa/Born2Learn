31cd3baaee1d353372b20d1eb73158fdb3623332
su <wkdlfflxh@naver.com>
2021-11-02 03:41:35 +0900

    Feat : add 다람쥐쥐

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import { React } from 'react';
+import Image from 'asset/sq.png';
 import './Header.scss';
 
 const Header = () => {
-  return <div className="header">WMPB</div>;
+  return (
+    <div className="header">
+      <div>WMPB</div>
+      <img
+        class="fit-picture"
+        src="asset/sq.png"
+        alt="Grapefruit slice atop a pile of other slices"
+      />
+    </div>
+  );
 };
 
 export default Header;
