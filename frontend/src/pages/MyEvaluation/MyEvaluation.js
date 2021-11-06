e2c44eea82a2a69571aa92866eda64b6e16f402e
su <wkdlfflxh@naver.com>
2021-11-06 20:07:53 +0900

    Feat : something change

--- frontend/src/pages/MyEvaluation/MyEvaluation.js
+++ frontend/src/pages/MyEvaluation/MyEvaluation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header, Footer } from 'components';
 
 import checkId from 'utils/checkId';
-import MyEvaluationContents from './MyEvaluationContents';
+// import MyEvaluationContents from './MyEvaluationContents';
 
 const MyEvaluation = ({ match, location }) => {
   const user_id = checkId(location);
@@ -12,7 +12,7 @@ const MyEvaluation = ({ match, location }) => {
       <Header user_id={user_id} />
       <div className="myevaluation-page ttemp">
         <div className="myevaluation-block">
-          <MyEvaluationContents />
+          {/* <MyEvaluationContents /> */}
         </div>
       </div>
       <Footer />
