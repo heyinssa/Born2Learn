393ccb216cb37d4fb3e5f81fa4570fec8978b178
su <wkdlfflxh@naver.com>
2021-11-29 23:30:34 +0900

    Feat : cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,8 +16,20 @@ const app = express();
 app.use(...expressLoader);
 app.use(cookieParser());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://dev.example.com:3000');
-  res.header('Access-Control-Allow-Credentials', true);
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+    );
+    res.header('Access-Control-Expose-Headers', 'Set-Cookie');
+    next();
+  });
   next();
 });
 app.use('/api', routes());
